cec8168c87f49a4134d8c3746e087c78442d7e15
wise <yannick.le-guennec@oclock.school>
2024-01-02 11:00:53 -0500

    Added the useWindowSize hook wich have for mission the ensure that our page/components always fit the window inner width and height of any devices

---
+++ src/hooks/useWindowSize/useWindowSize.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+    
+    handleResize();
+
+    // Nettoyer l'écouteur d'événement lors du démontage du composant
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
+  return windowSize;
+}
