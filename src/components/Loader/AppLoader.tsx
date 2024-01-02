f83a8aaa2111cc1eb697b92e687de8deefe9d39d
wise <yannick.le-guennec@oclock.school>
2024-01-02 10:44:11 -0500

    :bookmark: Added the Loader component + the MOdals folder that will contains all the future modals

---
+++ src/components/Loader/AppLoader.tsx
@@ -0,0 +1,41 @@
+// AppLoader.tsx
+// Developed by @yannick-leguennec
+
+import React, { CSSProperties } from 'react';
+import { Loader, Image, Text } from '@mantine/core';
+import logo from '../../public/img/FF_logo-homepage.png';
+import classes from './AppLoader.module.scss';
+
+/**
+ * AppLoader Component
+ * Displays a loading screen with a loader animation, title, and loading text.
+ */
+function AppLoader() {
+  const loaderStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  };
+
+  return (
+    <div style={loaderStyle}>
+      {/* Loader animation */}
+
+      <Image
+        src={logo}
+        className={`${classes.responsiveImage}`}
+        fit="contain"
+      />
+
+      {/* Loader animation */}
+      <Loader color="cyan" />
+
+      {/* Loading text */}
+      <Text style={{ marginTop: '10px' }}>Chargement en cours...</Text>
+    </div>
+  );
+}
+
+export default AppLoader;
